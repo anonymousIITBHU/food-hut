c6707637001347c38de9bc517d153e88a8d3a22f
anonymousIITBHU <78896308+anonymousIITBHU@users.noreply.github.com>
2024-05-27 19:11:13 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
     const handleSubmit = async(e)=>{
         e.preventDefault();
         // console.log(JSON.stringify({email:credentials.email, password:credentials.password}))
-        const response = await fetch("https://seven-spices-backend.onrender.com/api/loginuser",{
+        const response = await fetch("https://food-hut-back-v1.vercel.app/api/loginuser",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
